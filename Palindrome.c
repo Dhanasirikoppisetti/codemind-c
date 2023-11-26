6d4cb1b709a3a2af3a6e9e3c244a2c05d254a2b8
Dhanasirikoppisetti <147488134+Dhanasirikoppisetti@users.noreply.github.com>
2023-11-26 22:23:38 +0530

    #include<stdio.h> int main() {     int r,n,rev=0,t;     scanf("%d",&n);     t=n;     while(n!=0)     {       r=n%10;       rev=rev*10+r;       n=n/10;     }     if(t==rev)     {         printf("Palindrome");     }     else     {         printf("Not Palindrome");     } }

--- Palindrome.c
+++ Palindrome.c
@@ -1,21 +1,21 @@
 #include<stdio.h>
 int main()
 {
-    int n,r,i,rev=0,t;
+    int r,n,rev=0,t;
     scanf("%d",&n);
     t=n;
     while(n!=0)
     {
-        r=n%10;
-        rev=rev*10+r;
-        n=n/10;
+      r=n%10;
+      rev=rev*10+r;
+      n=n/10;
     }
     if(t==rev)
     {
-        printf("True");
+        printf("Palindrome");
     }
     else
     {
-        printf("False");
+        printf("Not Palindrome");
     }
 }
